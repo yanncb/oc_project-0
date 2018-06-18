34fe8d2988aa4db30e3a9f483f106ff130196529
yanncb <yann.caberas@gmail.com>
2018-06-18 10:16:41 +0200

    -Add 2 Products

--- src/main/java/com/lambazon/repository/ProductRepository.java
+++ src/main/java/com/lambazon/repository/ProductRepository.java
@@ -22,6 +22,10 @@ public class ProductRepository {
 		products.put(++id, new Product(id, 30, 69.99, "JVC HAFX8R Headphone", "Riptidz, In-Ear"));
 		products.put(++id, new Product(id, 40, 32.50, "VTech CS6114 DECT 6.0", "Cordless Phone"));
 		products.put(++id, new Product(id, 50, 895.00, "NOKIA OEM BL-5J", "Cell Phone "));
+		products.put(++id, new Product(id, 60, 100.00, "Beatsx", "Earphone")); //Add new product Beatsx
+		products.put(++id, new Product(id, 70, 250.00, "Beats Studio3 Wireless", "Head Phone")); // Add new product Beats Studio3
+
+
 	}
 	
 	public List<Product> products() {
