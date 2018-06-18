6d5f74e46edad15c4b7e0bc266ebd39531147a73
yanncb <yann.caberas@gmail.com>
2018-06-18 10:35:02 +0200

    - text formatting

--- src/main/java/com/lambazon/Application.java
+++ src/main/java/com/lambazon/Application.java
@@ -12,46 +12,44 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 /**
- * 
  * @author stanlick
- *
  */
 
 @SpringBootApplication
-public class Application implements  WebMvcConfigurer{
+public class Application implements WebMvcConfigurer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	// Internationalization (i18n)
+    // Internationalization (i18n)
 
-	@Bean
-	public LocaleResolver localeResolver() {
-		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-		localeResolver.setDefaultLocale(Locale.US);
-		return localeResolver;
-	}
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
 
-	/**
-	 * As all of our requests in the project are stateless, we must know, with each
-	 * incoming request that what locale should be served for this request. To
-	 * accomplish this, we need to define a LocaleChangeInterceptor bean
-	 * 
-	 * Apart from adding this bean, we also need to tell Spring to add this
-	 * interceptor at request level for all incoming requests.
-	 */
-	
-	   @Bean
-	   public LocaleChangeInterceptor localeChangeInterceptor() {
-	       LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-	       lci.setParamName("lang");
-	       return lci;
-	   }
-	 
-	   @Override
-	   public void addInterceptors(InterceptorRegistry registry) {
-	       registry.addInterceptor(localeChangeInterceptor());
-	   }
-	
+    /**
+     * As all of our requests in the project are stateless, we must know, with each
+     * incoming request that what locale should be served for this request. To
+     * accomplish this, we need to define a LocaleChangeInterceptor bean
+     * <p>
+     * Apart from adding this bean, we also need to tell Spring to add this
+     * interceptor at request level for all incoming requests.
+     */
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
 }
